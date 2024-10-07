3d76afbe47ef8028f0c533dbaa0fc7fed28e8fa6
mahmoodfathy227 <mahmoodfathy247@gmail.com>
2024-10-07 12:30:14 +0300

    help and product modules

--- lib/app/modules/global/config/helpers.dart
+++ lib/app/modules/global/config/helpers.dart
@@ -1,4 +1,11 @@
 
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter/material.dart';
 
+import '../theme/app_theme.dart';
 final kDefaultPadding = 16.0.w;
+TextStyle errorTextStyle = secondaryTextStyle(color: Colors.red,
+    size: 12.sp.round(),
+    weight: FontWeight.w300
+
+);
